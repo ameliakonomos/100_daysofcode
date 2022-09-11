d96ac2889d06ff85bd082e9fd6c1df67f2df502d
ameliakonomos <103978227+ameliakonomos@users.noreply.github.com>
2022-09-11 06:38:21 -0700

    Add files via upload

---
+++ projects/day17/question_model.py
@@ -0,0 +1,8 @@
+#for question object
+#text and answer attributes
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+#create question bank on question objects
