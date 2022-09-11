c4759cdeaee29c3946bb54f8d9ffa79631053107
ameliakonomos <103978227+ameliakonomos@users.noreply.github.com>
2022-09-11 06:38:39 -0700

    Add files via upload

---
+++ projects/day17/mainquiz.py
@@ -0,0 +1,16 @@
+#creating quiz game
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = [] #list of question objects
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
