f78dc23c6d9d842b636bb8e7e3a5a2e038a40db9
ameliakonomos <103978227+ameliakonomos@users.noreply.github.com>
2022-09-12 07:39:34 -0700

    Add files via upload

---
+++ projects/day18/spirograph.py
@@ -0,0 +1,38 @@
+#python tuples
+#rgb color is represented by a tuple
+#tuple = data type
+#(1,3,8) = tuple
+#it is similar to a list
+#values in tuple cannot be changed, unlike lists
+import turtle as t
+import random
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+tim.speed("fastest")
+def draw_spirograph(size_of_gap):
+    for i in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spirograph(5)
+#
+# for i in range(100):
+#     tim.color(random_color())
+#     tim.circle(100)
+#     current_heading = tim.heading()
+#     tim.setheading(current_heading + 10)
+#     tim.circle(100)
+
+
+
+screen = t.Screen()
+screen.exitonclick()
