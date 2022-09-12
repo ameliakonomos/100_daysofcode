2f92a093ae692c288c7a525469b2b00e9c30458c
ameliakonomos <103978227+ameliakonomos@users.noreply.github.com>
2022-09-12 07:53:25 -0700

    Add files via upload

---
+++ projects/day18/main2.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+import turtle as t
+import random
+tim = t.Turtle()
+
+colors = ["DarkOrchid", "IndianRed", "DeepSkyBlue"]
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
+
+draw_shape(10)
+
+
+
+
+
+
